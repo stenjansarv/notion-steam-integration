0d476b5f22e6154a8e45cac0890d74ae66c2188b
Sten-Jan Sarv <stenjansarv@gmail.com>
2024-07-13 22:38:52 +0300

    chore: add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import stylisticPlugin from '@stylistic/eslint-plugin'
+import typescriptParser from '@typescript-eslint/parser'
+
+export default [
+  {
+    languageOptions: {
+      parser: typescriptParser,
+    },
+    plugins: {
+      '@stylistic': stylisticPlugin,
+    },
+    files: ['**/*.{js,ts}'],
+    rules: {
+      '@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/indent-binary-ops': ['error', 2],
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/array-bracket-newline': ['error', 'consistent'],
+      '@stylistic/array-bracket-spacing': ['error', 'never'],
+      '@stylistic/block-spacing': ['error', 'always'],
+      '@stylistic/comma-style': ['warn', 'last'],
+      '@stylistic/dot-location': ['error', 'property'],
+      '@stylistic/function-call-spacing': ['error', 'never'],
+      '@stylistic/eol-last': ['error', 'always'],
+
+      '@stylistic/key-spacing': ['error', { 'beforeColon': false, 'afterColon': true }],
+
+      'prefer-const': 'error',
+    },
+  },
+]
